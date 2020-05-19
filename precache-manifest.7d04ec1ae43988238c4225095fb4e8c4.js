149fa1246354c9f28bc3a800db4fabfef672b048
Terran Kroft <terrankroft@gmail.com>
2020-05-19 13:27:25 +0800

    Updates

---
+++ precache-manifest.7d04ec1ae43988238c4225095fb4e8c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a018172bdb5eb8fd00daecf7d49abc",
+    "url": "/pokecatch/index.html"
+  },
+  {
+    "revision": "1d9fdda5bee019837d03",
+    "url": "/pokecatch/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "abf5fa29ee94e8f4ce0c",
+    "url": "/pokecatch/static/js/2.dfb938e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokecatch/static/js/2.dfb938e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9fdda5bee019837d03",
+    "url": "/pokecatch/static/js/main.e316033a.chunk.js"
+  },
+  {
+    "revision": "0e11add886484bee88c1",
+    "url": "/pokecatch/static/js/runtime-main.e619edd2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokecatch/static/media/logo.5d5d9eef.svg"
+  }
+]);
